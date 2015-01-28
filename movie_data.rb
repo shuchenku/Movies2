d398a79f6e1b16b6135f714f3a0038b0c11900e6
SHU CHEN <chenshu8805@gmail.com>
2015-01-27 21:33:17 -0500

    2.5th commit

--- movie_data.rb
+++ movie_data.rb
@@ -1,5 +1,3 @@
-load './movie_test.rb'
-
 class MovieData
 
 	attr_accessor :datahash
@@ -212,13 +210,53 @@ class MovieData
 end
 
 
-test = MovieData.new('ml-100k',:u1)
-test_obj = test.run_test()
+class MovieTest
 
-puts "mean err: #{test_obj.mean}"
-puts "stddev: #{test_obj.stddev}"
-puts "rms: #{test_obj.rms}"
-puts "Array size #{test_obj.to_a.size}X#{test_obj.to_a[0].size}"
+	def initialize(predictions,test_data)
+		@u = test_data.transpose[0]
+		@m = test_data.transpose[1]
+		@r = test_data.transpose[2]
+		@p = predictions
+		@test_data = test_data
+
+	end
+
+	def mean()
+		diff = (0..@p.size-1).inject(0) {|sum,el|
+			sum + (@r[el] - @p[el]).abs
+		}
+		return mean_err = diff.to_f/@p.size
+	end
+
+	def stddev
+		avgerr = mean()
+		sqrd = (0..@p.size-1).inject(0) {|sum,el|
+			sum + ((@r[el] - @p[el]).abs-avgerr)**2
+		}
+		return stdderr = Math::sqrt(sqrd.to_f/@p.size)
+	end
+
+	def rms
+		sqrd = (0..@p.size-1).inject(0) {|sum,el|
+			sum + (@r[el] - @p[el])**2
+		}
+		return rms = Math::sqrt(sqrd.to_f/@p.size)		
+	end
+
+	def to_a
+		return (@test_data[(0..@p.size-1)].transpose[(0..2)] << @p).transpose
+	end
+
+end
+
+
+# test = MovieData.new('ml-100k',:u1)
+# test_obj = test.run_test()
+
+# puts "mean err: #{test_obj.mean}"
+# puts "stddev: #{test_obj.stddev}"
+# puts "rms: #{test_obj.rms}"
+# puts "Array size #{test_obj.to_a.size}X#{test_obj.to_a[0].size}"
 
 # 	  Pearson     Cosine
 #     0.5 cutoff  0.3 cutoff
